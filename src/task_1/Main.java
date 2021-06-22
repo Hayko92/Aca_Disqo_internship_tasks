5e8367f2b68af1650ce96445452891a06f54f69e
Hayk <hajko92@mail.ru>
2021-06-22 11:11:17 +0400

    Done

--- src/task_1/Main.java
+++ src/task_1/Main.java
@@ -8,7 +8,7 @@ public class Main {
  Point p1 = new Point(6,6);
         System.out.println(p1.distance());
         System.out.println(p1.distance(new Point(1,1)));
-        System.out.println(new Random().nextInt(11));
+
 
 
 
